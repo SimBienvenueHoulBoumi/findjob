32fc4a8cab6a8b9e2d24f406ad1f61f464a20170
Sim <brhulla@gmail.com>
2025-01-18 14:32:19 +0100

    🎉 Jobfinder initialiation
    first commit

---
+++ src/main/java/simdev/finderjob/FinderjobApplication.java
@@ -0,0 +1,13 @@
+package simdev.finderjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simdev/finderjob/FinderjobApplicationTests.java
@@ -0,0 +1,13 @@
+package simdev.finderjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
